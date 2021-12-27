bdea381b0f17dc0ec373aa35be62b1c4874f6eba
Tony Zharoff <tony.zharoff@gmail.com>
2021-12-27 09:09:12 -0800

    Added the ability to have collectibles and a collection manager.

---
+++ Assets/Scripts/CollectionManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e797a461c60a4b0eec9b27bd64017a26d5fb44ac94a24f6c05a9b91d27812b7
+size 3145
---
+++ Assets/Scripts/CubeCollectible.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d8cf5467c9a8216c37e55f3aa85088f990d9a774ceb09a3c974b4c447baa2478
+size 340
